49e6e44c28c144c94b85c335b6421bfd9c881b1a
Muhammad Tomasiewicz <abdullahimuhammad269@gmail.com>
2020-05-15 22:22:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMenu() {
+  var x = document.getElementById("mobile");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
