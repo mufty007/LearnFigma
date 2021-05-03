400701b178f60519635c95f291b51cb27426a5bd
Muhammad Tomasiewicz <abdullahimuhammad269@gmail.com>
2021-05-02 21:16:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showMenu() {
-  var x = document.getElementById("mobile");
+  var x = document.getElementById("menu_mobile");
   if (x.style.display === "none") {
     x.style.display = "flex";
   } else {
